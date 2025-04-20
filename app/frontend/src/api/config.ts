210a2a2a6e719f265fc1d3e2768dd5a7474bf1f1
Ivan Radovic <ivanrdvc@gmail.com>
2025-04-20 20:54:18 +0200

    Updated vite configuration for api base url

--- app/frontend/src/api/config.ts
+++ app/frontend/src/api/config.ts
@@ -1,3 +1,3 @@
 export const API_CONFIG = {
-    BACKEND_URI: import.meta.env.VITE_BACKEND_URI || "https://localhost:7019"
+    BACKEND_URI: import.meta.env.VITE_API_BASE_URL || "https://localhost:7019"
 };
